b1cbd2bb39fd23eba55ea9ba006813b7f4ac774d
Ethan Dalool <edalool@yahoo.com>
2017-03-30 15:53:48 -0700

    Update voussoirkit

--- _voussoirkit/phase1.py
+++ _voussoirkit/phase1.py
@@ -4,16 +4,18 @@ import os
 PATHS = [
 'C:\\git\\else\\BaseNumber\\basenumber.py',
 'C:\\git\\else\\Bytestring\\bytestring.py',
+'C:\\git\\else\\Cacheclass\\cacheclass.py',
 'C:\\git\\else\\Clipext\\clipext.py',
 'C:\\git\\else\\Downloady\\downloady.py',
+'C:\\git\\else\\ExpressionMatch\\expressionmatch.py',
 'C:\\git\\else\\Fusker\\fusker.py',
 'C:\\git\\else\\Pathclass\\pathclass.py',
 'C:\\git\\else\\Ratelimiter\\ratelimiter.py',
 'C:\\git\\else\\RateMeter\\ratemeter.py',
 'C:\\git\\else\\Safeprint\\safeprint.py',
-'C:\\git\\else\\Treeclass\\treeclass.py',
-'C:\\git\\else\\Treeclass\\pathtree.py',
 'C:\\git\\else\\SpinalTap\\spinal.py',
+'C:\\git\\else\\Treeclass\\pathtree.py',
+'C:\\git\\else\\Treeclass\\treeclass.py',
 ]
 
 os.makedirs('voussoirkit', exist_ok=True)
--- _voussoirkit/setup.py
+++ _voussoirkit/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='voussoirkit',
     packages=['voussoirkit'],
-    version='0.0.9',
+    version='0.0.11',
     author='voussoir',
     author_email='ethan@voussoir.net',
     description='voussoir\'s toolkit',
