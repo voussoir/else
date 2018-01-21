fbf64a50c5d1d189a631c5814ec2b1474493e036
Ethan Dalool <edalool@yahoo.com>
2018-01-21 14:24:11 -0800

    Update voussoirkit with fixed pathclass.

--- _voussoirkit/setup.py
+++ _voussoirkit/setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name='voussoirkit',
     packages=['voussoirkit'],
-    version='0.0.18',
+    version='0.0.19',
     author='voussoir',
     author_email='ethan@voussoir.net',
     description='voussoir\'s toolkit',
